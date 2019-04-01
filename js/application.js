224ed3d9a65ed34a2966f13f89ae9f4708db6140
tgeant <tancrede.geant@protegys.com>
2019-04-01 15:23:04 +0200

    Add conversion + button state

--- js/application.js
+++ js/application.js
@@ -1,3 +1,24 @@
+const json = {
+  "string": "This is a string",
+  "test": {element:"random", fries: "potato"},
+  "It's an array": [
+    {
+      "lol": "pwet",
+      "banana": 4,
+      "isFruit": true
+    },
+    {
+      "otherElement": {
+        "i am another element": "yup"
+      },
+      "expect": "nothing"
+    }
+    
+  ]
+};
+
+
+
 // ---------------- VIEW.JS -------------------
 Vue.use(Buefy.default);
 
@@ -12,7 +33,8 @@ Vue.component('node-tree', {
                   <button
                   class="button is-primary is-small"
                   slot="trigger"
-                  aria-controls="contentIdForA11y1">+</button>
+                  aria-controls="contentIdForA11y1"
+                  @click="node.isOpen = !node.isOpen">{{ node.isOpen? '-' : '+' }}</button>
                     <ul v-if="node.children.length">
                         <node-tree v-for="child in node.children" :node="child"></node-tree>
                     </ul>
@@ -51,25 +73,84 @@ Vue.component('node-tree', {
             return {
                 tree: {
                     label: "A cool folder",
+                    isOpen: false,
                     children: [
                       {
                         label: "A cool sub-folder 1",
+                        isOpen: false,
                         children: [
-                          { label: "A cool sub-sub-folder 1", children : [{label: "test"}] },
+                          { label: "A cool sub-sub-folder 1",  isOpen: false, children : [{label: "test"}] },
                           { label: "A cool sub-sub-folder 2", children: "value", isError:true }
                         ]
                       },
                       { label: "This one is not that cool" }
                     ]
-                  }
+                  },
+                tree2 : {}
             }
         },
         methods: {
-          
+          customTree(){
+            this.tree2 = createJsonToArray(json, "results");
+            return this.tree2;
+          }
         }
     }
 
 const app = new Vue(example)
 
 app.$mount('#app')
-            
+
+
+
+
+
+function createJsonToArray(json, name){
+  var element = {};
+
+  element.label = name;
+  element.children = convert_to_array_json(json);
+  element.isOpen = false;
+
+  return element;
+}
+
+
+
+function convert_to_array_json(json){
+
+  
+  var json_result = [];
+
+  for(var item in json){
+
+    var element = {};
+    element.label = item;
+    element.children = json[item];
+    element.isOpen = false;
+
+    if(Array.isArray(json[item])){
+
+      element.children = [];
+
+      for(var i=0; i< json[item].length; i++){
+        var ele = {};
+        ele.label = ""+i;
+        ele.children = convert_to_array_json(json[item][i]);
+        ele.isOpen = false;
+
+        element.children.push(ele);
+      }
+      
+    } else if(isNaN(element.children) && !(typeof element.children === 'boolean' || element.children instanceof Boolean) &&!(typeof element.children === 'string' || element.children instanceof String) ){
+        element.children = convert_to_array_json(element.children);
+  }
+    
+
+    json_result.push(element);
+  }
+
+  return json_result;
+}
+
+      
